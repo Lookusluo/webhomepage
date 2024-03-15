a7d9f19711ce292cc740aa69284c7cea38d65f8c
Lucas <Lookusluo@users.noreply.github.com>
2024-03-16 00:04:43 +0800

    old version

--- static/script.js
+++ static/script.js
@@ -1,37 +1,14 @@
-console.log('%cCopyright © 2024 zyyo.net',
-    'background-color: #ff00ff; color: white; font-size: 24px; font-weight: bold; padding: 10px;'
-);
-console.log('%c   /\\_/\\', 'color: #8B4513; font-size: 20px;');
-console.log('%c  ( o.o )', 'color: #8B4513; font-size: 20px;');
-console.log(' %c  > ^ <', 'color: #8B4513; font-size: 20px;');
-console.log('  %c /  ~ \\', 'color: #8B4513; font-size: 20px;');
-console.log('  %c/______\\', 'color: #8B4513; font-size: 20px;');
 
-document.addEventListener('contextmenu', function (event) {
-    event.preventDefault();
-});
 
-function handlePress(event) {
-    this.classList.add('pressed');
-}
+document.onkeydown = function (e) {
+    if (e.key === "F12") {
+        e.preventDefault();
+    }
+};
 
-function handleRelease(event) {
-    this.classList.remove('pressed');
-}
-
-function handleCancel(event) {
-    this.classList.remove('pressed');
-}
-
-var buttons = document.querySelectorAll('.projectItem');
-buttons.forEach(function (button) {
-    button.addEventListener('mousedown', handlePress);
-    button.addEventListener('mouseup', handleRelease);
-    button.addEventListener('mouseleave', handleCancel);
-    button.addEventListener('touchstart', handlePress);
-    button.addEventListener('touchend', handleRelease);
-    button.addEventListener('touchcancel', handleCancel);
-});
+document.body.oncontextmenu = function (e) {
+    e.preventDefault();
+};
 
 function toggleClass(selector, className) {
     var elements = document.querySelectorAll(selector);
@@ -41,26 +18,118 @@ function toggleClass(selector, className) {
 }
 
 function wx(imageURL) {
+    toggleClass(".tc", "active");
+    toggleClass(".tc-main", "active");
+
     var tcMainElement = document.querySelector(".tc-img");
     if (imageURL) {
         tcMainElement.src = imageURL;
     }
-    toggleClass(".tc-main", "active");
-    toggleClass(".tc", "active");
 }
 
-var tc = document.getElementsByClassName('tc');
-var tc_main = document.getElementsByClassName('tc-main');
-tc[0].addEventListener('click', function (event) {
-    wx();
-});
-tc_main[0].addEventListener('click', function (event) {
-    event.stopPropagation();
-});
 
-function left() {
-    toggleClass(".left-main", "left-main-open");
-    toggleClass(".left", "left-open");
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+   var themeState = getCookie("themeState") || "Blue"; 
+    var svgItems = document.getElementsByTagName("svg");
+    var tanChiShe = document.getElementById("tanChiShe");
+    var body = document.body;
+    function changeSvg(color) {
+        for (var i = 0; i < svgItems.length; i++) {
+            var paths = svgItems[i].getElementsByTagName("path");
+            for (var j = 0; j < paths.length; j++) {
+                paths[j].setAttribute("fill", color);
+            }
+        }
+    }
+    function changeTheme(theme) {
+        if (theme == "Light") {
+           themeState = "Light";
+            changeSvg("#000000");
+            tanChiShe.src = "/static/svg/snake-Light.svg";
+            var lightStyles = {
+                '--main-bg-color': '#ffffff',
+                '--main-text-color': '#000000',
+                '--gradient-start': '#607df1',
+                '--gradient-middle': '#e0321b',
+                '--gradient-end': '#000000',
+                '--purple-text-color': '#2b3ce2',
+                '--text-bg-color': '#f4f4f4',
+                '--icon-bg-color': 'rgb(246 246 246)',
+                '--icon-1-hover-color': 'rgb(68, 120, 241)',
+                '--icon-2-hover-color': 'rgb(232, 68, 241)',
+                '--icon-3-hover-color': 'rgb(179, 206, 0)',
+                '--icon-4-hover-color': 'rgb(201, 13, 0)',
+                '--icon-5-hover-color': 'rgb(111, 44, 20)',
+                '--project-item-bg-color': 'rgb(246 246 246)',
+                '--project-item-hover-color': '#eeeeee',
+                '--project-item-left-title-color': '#000000',
+                '--project-item-left-text-color': '#7e7e7e'
+            };
+            for (const property in lightStyles) {
+                body.style.setProperty(property, lightStyles[property]);
+            }
+
+        } else if (theme == "Dark") {
+             themeState = "Dark";
+            changeSvg("#ffffff");
+            tanChiShe.src = "/static/svg/snake-Dark.svg";
+            const dackStyles = {
+                '--main-bg-color': 'rgb(0, 0, 0)',
+                '--main-text-color': '#ffffff',
+                '--gradient-start': 'rgb(133, 62, 255)',
+                '--gradient-middle': '#f76cc6 30%',
+                '--gradient-end': 'rgb(255, 255, 255) 60%',
+                '--purple-text-color': 'rgb(115, 19, 206)',
+                '--text-bg-color': 'rgb(26, 4, 48)',
+                '--icon-bg-color': 'rgb(19 20 24)',
+                '--icon-1-hover-color': 'rgb(68, 120, 241)',
+                '--icon-2-hover-color': 'rgb(232, 68, 241)',
+                '--icon-3-hover-color': 'rgb(179, 206, 0)',
+                '--icon-4-hover-color': 'rgb(201, 13, 0)',
+                '--icon-5-hover-color': 'rgb(111, 44, 20)',
+                '--project-item-bg-color': 'rgb(19 20 24)',
+                '--project-item-hover-color': 'rgb(19, 23, 27)',
+                '--project-item-left-title-color': 'rgb(255, 255, 255)',
+                '--project-item-left-text-color': 'rgb(142, 142, 142)',
+                '--footer-text-color': '#646464'
+            };
+
+            for (const property in dackStyles) {
+                body.style.setProperty(property, dackStyles[property]);
+            }
+        }else if (theme == "Blue") {
+ themeState = "Blue";
+            changeSvg("#000000");
+            tanChiShe.src = "/static/svg/snake-Light.svg";
+            var blueStyles = {
+                '--main-bg-color': ' linear-gradient(45deg, #7fb2e5, white)',
+                '--main-text-color': '#000000',
+                '--gradient-start': '#607df1',
+                '--gradient-middle': '#e0321b',
+                '--gradient-end': '#000000',
+                '--purple-text-color': '#2b3ce2',
+                '--text-bg-color': '#f4f4f4',
+                '--icon-bg-color': 'rgba(249, 250, 251, 0.6)',
+                '--icon-1-hover-color': 'rgb(68, 120, 241)',
+                '--icon-2-hover-color': 'rgb(232, 68, 241)',
+                '--icon-3-hover-color': 'rgb(179, 206, 0)',
+                '--icon-4-hover-color': 'rgb(201, 13, 0)',
+                '--icon-5-hover-color': 'rgb(111, 44, 20)',
+                '--project-item-bg-color': 'rgba(249, 250, 251, 0.6)',
+                '--project-item-hover-color': 'rgba(240, 241, 241, 0.6)',
+                '--project-item-left-title-color': '#000000',
+                '--project-item-left-text-color': '#7e7e7e'
+            };
+            for (const property in blueStyles) {
+                body.style.setProperty(property, blueStyles[property]);
+            }
+
+        }
+       setCookie("themeState", theme, 365);
 }
 
 function setCookie(name, value, days) {
@@ -72,6 +141,7 @@ function setCookie(name, value, days) {
     }
     document.cookie = name + "=" + value + expires + "; path=/";
 }
+
 
 function getCookie(name) {
     var nameEQ = name + "=";
@@ -87,144 +157,55 @@ function getCookie(name) {
     }
     return null;
 }
+changeTheme(themeState);
+   
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
-
-
-
-
-
-
-    var html = document.querySelector('html');
-    var themeState = getCookie("themeState") || "Light";
-    var svgItems = document.getElementsByTagName("svg");
-    var tanChiShe = document.getElementById("tanChiShe");
-
-
-
-
-
-
-    function changeTheme(theme) {
-
-        tanChiShe.src = "/static/svg/snake-" + theme + ".svg";
-        //切换贪吃蛇
-        html.dataset.theme = theme;
-        //设置css变量主题
-        //不可调换位置,必须在设置css变量后,获取当前css变量的svgcolor
-        var svgcolor = getComputedStyle(document.documentElement).getPropertyValue('--svgcolor')
-        //获取当前主题svg颜色值
-        for (var i = 0; i < svgItems.length; i++) {
-            var paths = svgItems[i].getElementsByTagName("path");
-            for (var j = 0; j < paths.length; j++) {
-                paths[j].setAttribute("fill", svgcolor);
-            }
-        }
-        setCookie("themeState", theme, 365);
-        themeState = theme;
+    const switchCheckbox = document.getElementById('myonoffswitch');
+    /*夜间自动打开暗色主题
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+    if (currentHour >= 18 || currentHour < 6) {
+        switchCheckbox.checked = false;
+        changeTheme(1);
     }
+  */
 
-
-
-
-
-
-
-    var Checkbox = document.getElementById('myonoffswitch')
-    Checkbox.addEventListener('change', function () {
-        if (themeState == "Dark") {
-            changeTheme("Light");
-        } else if (themeState == "Light") {
-            changeTheme("Dark");
-        } else {
-            changeTheme("Dark");
-        }
-    });
-
-
-
-    if (themeState == "Dark") {
-        Checkbox.checked = false;
-    }
-
-    changeTheme(themeState);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    var pageLoading = document.querySelector("#zyyo-loading");
-    setTimeout(function () {
-        pageLoading.style.opacity = '0';
-
-    }, 100);
-
-
-    var fpsElement = document.createElement('div');
-    fpsElement.id = 'fps';
-    fpsElement.style.zIndex = '10000';
-    fpsElement.style.position = 'fixed';
-    fpsElement.style.left = '0';
-    document.body.insertBefore(fpsElement, document.body.firstChild);
-
-    var showFPS = (function () {
-        var requestAnimationFrame = window.requestAnimationFrame ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
-            window.oRequestAnimationFrame ||
-            window.msRequestAnimationFrame ||
-            function (callback) {
-                window.setTimeout(callback, 1000 / 60);
-            };
-
-        var fps = 0,
-            last = Date.now(),
-            offset, step, appendFps;
-
-        step = function () {
-            offset = Date.now() - last;
-            fps += 1;
-
-            if (offset >= 1000) {
-                last += offset;
-                appendFps(fps);
-                fps = 0;
-            }
-
-            requestAnimationFrame(step);
-        };
-
-        appendFps = function (fpsValue) {
-            fpsElement.textContent = 'FPS: ' + fpsValue;
-        };
-
-        step();
-    })();
+switchCheckbox.addEventListener('change', function () {
+   if (themeState == "Light"){
+       
+       changeTheme("Blue");
+   }else if(themeState == "Dark"){
+    
+       changeTheme("Light");
+       
+   }else if(themeState == "Blue"){
+       
+       changeTheme("Dark");
+   }
 });
 
+
+    var projectItems = document.querySelectorAll(".projectItem");
+
+    function checkProjectItems() {
+        for (var i = 0; i < projectItems.length; i++) {
+            var projectItem = projectItems[i];
+            var projectItemTop = projectItem.getBoundingClientRect().top;
+
+            if (projectItemTop < window.innerHeight * 1.05) {
+                projectItem.classList.add("fade-in-visible");
+            }
+        }
+    }
+
+    window.addEventListener("scroll", checkProjectItems);
+    window.addEventListener("resize", checkProjectItems);
+
+    checkProjectItems();
+
+
+
+
+});
